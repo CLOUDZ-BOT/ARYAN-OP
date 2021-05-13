2cff07ae359fdcedd29bbe547a8e726510d287ed
SHIVAM0001-FLEX <80771521+SHIVAM0001-FLEX@users.noreply.github.com>
2021-05-13 23:13:59 +0530

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -21,7 +21,7 @@ module.exports = {
     .setTitle(`Invite Links of Cloudz Music`)
     .addField("<a:discordgg:842055987732152371>**__Invite Me With Admin Perms__**", `<a:766034662589202502:842056330998054963> **Cloudz 1 -** ${av1}\n<a:766034662589202502:842056330998054963> **Cloudz 2 -** ${av2}\n<a:766034662589202502:842056330998054963> **Cloudz 3 -** ${av3}`)
     .addField("<a:discordgg:842055987732152371> **__Invite Me Without Admin Perms__**", `<a:766034662589202502:842056330998054963> **Cloudz 1 -** ${nav1}\n<a:766034662589202502:842056330998054963> **Cloudz 2 -** ${nav2}\n<a:766034662589202502:842056330998054963> **Cloudz 3 -** ${nav3}`)
-    .addField("<<a:discordgg:842055987732152371> **__Support Server__**", `<a:766034662589202502:842056330998054963> **[Join Now](https://discord.gg/zqMJxmz67T)**`)
+    .addField("<a:discordgg:842055987732152371> **__Support Server__**", `<a:766034662589202502:842056330998054963> **[Join Now](https://discord.gg/zqMJxmz67T)**`)
     .setColor("#ff0000")
     return message.channel.send(invite);
   }
