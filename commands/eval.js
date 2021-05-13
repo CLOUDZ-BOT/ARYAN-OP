baeb1faec1c25ab4767822763ca0ebf78e3490d1
SHIVAM0001-FLEX <80771521+SHIVAM0001-FLEX@users.noreply.github.com>
2021-05-14 02:18:28 +0530

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,35 +1,31 @@
-if(message.content.startsWith(`eval`)){
+const Discord = require("discord.js");
 
-const notowner = new Discord.MessageEmbed()
-.setDescription("Only the bot owner can use this command")
-.setColor("#ff0000")
-const owners_id = ["764546128204005436"];
- if (!owners_id.includes(message.author.id))
- return message.channel.send(notowner); const args2 = message.content.split(" ").slice(1);
-
- const clean = text => {
- if (typeof(text) === "string")
- return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
- else
- return text;
-}
+module.exports.run = async (bot, message, args, color, prefix) => {
+    if (message.author.id !== '764546128204005436') {
+   message.reply("Only My Owner Can Use This")
+ } else {
+     try {
+         let codein = args.join(" ");
+         let code = eval(codein);
  
- try {
- const code = args2.join(" ");
- let evaled = eval(code);
-const lmao = message.content.slice("".length).trim().split(/ +/);
-lmao.shift().toLowerCase().split(" ")[0]
-msg.channel.send(lmao.join(" "))
- const { inspect } = require("util");
-const output = clean(evaled)
-
- const eval2 = new Discord.MessageEmbed()
- .addField("Input", `\`\`\`js\n${lmao.join(" ")}\`\`\``)
- .addField("Output", `\`\`\`js\n${output}\`\`\``)
- 
- // msg.channel.send(clean(evaled));
- message.channel.send(eval2)
- } catch (err) {
- message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+         if (typeof code !== 'string')
+             code = require('util').inspect(code, { depth: 0 });
+         let embed = new Discord.RichEmbed()
+         .setAuthor('Evaluate')
+         .setColor("#ff0000")
+         .addField('📥 Input', `\`\`\`js\n${codein}\`\`\``)
+         .addField('📤 Output', `\`\`\`js\n${code}\n\`\`\``)
+         .setTimestamp() 
+         message.channel.send(embed)
+     } catch(e) {
+         message.channel.send(`\`\`\`js\n${e}\n\`\`\``);
+     }
+    }
  }
-};
+
+exports.help = {
+    name: 'eval',
+    aliases: ['ev'],
+    description: 'only my onwer can use this command',
+    usage: '{prefix}eval [some javascript code]'
+}
