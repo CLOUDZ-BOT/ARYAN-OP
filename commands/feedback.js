201c6150848e3e1454d32d882eda8d5a694b6720
SHIVAM0001-FLEX <80771521+SHIVAM0001-FLEX@users.noreply.github.com>
2021-05-13 10:33:57 +0530

    Update feedback.js

--- commands/feedback.js
+++ commands/feedback.js
@@ -1,4 +1,5 @@
 const {MessageEmbed} = require('discord.js')
+const { EMOJI_DONE , BOT_ID , CHECK_DM , EMBED_COLOR } = require('../config.json');
 module.exports = {
     name: "feedback",
     aliases: ["fb"],
@@ -6,7 +7,7 @@ module.exports = {
     description: "submit a feedback",
     async execute(message, args) {
 
-        const fbChannel = "839478605209010207";
+        const fbChannel = "822580249728122891";
 
         const fb = args.join(" ");
         if(!fb){
@@ -14,25 +15,29 @@ module.exports = {
         }
 
         const embed = new MessageEmbed()
-            .setTitle("Cloudz Music Feedback <a:astroz_success:839478588192718898>")
+            .setTitle("Astroz Music Feedback :astroz_correct:")
             .addField(`Author`, `\`${message.author.tag}\``)
             .addField(`Feedback`, "\`"+fb+"\`")
             .addField(`Member Id`, `\`${message.author.id}\``)
             .addField(`On the Server`, `\`${message.guild.name}\``)
             .addField("Server ID:", `\`${message.guild.id}\``)
-            .setColor("#FF0000")
+            .setColor(EMBED_COLOR)
             .setTimestamp()
                     
         message.client.channels.cache.get(fbChannel).send(embed).then((msg) =>{
+            msg.react(':astroz_correct:');
+            msg.react(':astroz_wrong:');
         }).catch((err)=>{
             throw err;
         });
 
 
         const successembed = new MessageEmbed()
+        .addField("Join Our Support Server", `Click Here To Join Our [Support Server](https://discord.gg/m7W2t3bsZj)`)
         .setTitle("Success!")
-        .setColor("#FF0000")
-        .setDescription(` Your **Feedback** is submitted successfully!`)
+        .setColor(EMBED_COLOR)
+        .setDescription(`${EMOJI_DONE} Your **Feedback** is submitted successfully!`)
+        message.react(EMOJI_DONE);
 
         message.author.send(successembed)
     }
