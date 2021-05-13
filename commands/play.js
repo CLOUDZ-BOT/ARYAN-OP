bcb42a5e4229cb3495afff3a677e395ea5ac1b56
SHIVAM0001-FLEX <80771521+SHIVAM0001-FLEX@users.noreply.github.com>
2021-05-13 22:35:38 +0530

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -88,7 +88,7 @@ module.exports = {
         if (serverQueue) {
             serverQueue.songs.push(song);
             let thing = new MessageEmbed()
-                .setAuthor("Song has been added to queue", "https://images-ext-1.discordapp.net/external/k3fef3p4I2XTIUN4tykUa_4XKHc_WHrG5-1uiKbJR98/https/cdn.discordapp.com/avatars/822294569223454740/06d90b47826d04bc369fef70fd8b515a.webp")
+                .setAuthor("Song has been added to queue", "https://cdn.discordapp.com/attachments/821815886524448799/842446347172184065/Music.gif")
                 .setColor("#FF0000")
                 .addField("Name", song.title, true)
                 .addField("Duration", song.duration, true)
