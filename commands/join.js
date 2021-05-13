a130aebaea22f1200beb4111d9026e7d0fb92cee
SHIVAM0001-FLEX <80771521+SHIVAM0001-FLEX@users.noreply.github.com>
2021-05-13 10:31:12 +0530

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -12,13 +12,13 @@ module.exports = {
 		const vc = new MessageEmbed()
 		.setDescription(`You're not in a voice channel!`)
 		.setTitle("Error!")
-		.setColor("#fe0000")
+		.setColor("#ff0000")
     if (!message.member.voice.channel) return message.channel.send(vc);
 
       		const nsvc = new MessageEmbed()
 		.setDescription(`<@${client.user.id}> **Is Already Playing Music In Other VC**!`)
 		.setTitle("Error!")
-		.setColor("#fe0000")
+		.setColor("#ff0000")
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(nsvc);
 
 		const svc = new MessageEmbed()
