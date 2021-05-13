b97a306f0bb14881f14e74be5022b5abe5a489eb
SHIVAM0001-FLEX <80771521+SHIVAM0001-FLEX@users.noreply.github.com>
2021-05-13 10:16:16 +0530

    Update error.js

--- util/error.js
+++ util/error.js
@@ -7,7 +7,7 @@ const { MessageEmbed } = require("discord.js")
  */
 module.exports = async (text, channel) => {
     let embed = new MessageEmbed()
-    .setColor("RED")
+    .setColor("#FF0000")
     .setDescription(text)
     .setFooter("Something went wrong :(")
     await channel.send(embed)
