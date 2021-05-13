663eaed1616933cd18bd03305844586bab12e395
SHIVAM0001-FLEX <80771521+SHIVAM0001-FLEX@users.noreply.github.com>
2021-05-13 10:41:13 +0530

    Update bugreport.js

--- commands/bugreport.js
+++ commands/bugreport.js
@@ -1,16 +1,17 @@
 const {MessageEmbed} = require('discord.js')
+
 module.exports = {
     name: "bugreport",
     aliases: ["br"],
     cooldown: 3,
-    description: "submit a bug",
+    description: "submit a Bug",
     async execute(message, args) {
 
-        const fbChannel = "839478606669283328";
+        const fbChannel = "839478605209010207";
 
         const fb = args.join(" ");
         if(!fb){
-            return message.channel.send("Please enter a bug!");
+            return message.channel.send("Please enter a Bug!");
         }
 
         const embed = new MessageEmbed()
@@ -20,19 +21,23 @@ module.exports = {
             .addField(`Member Id`, `\`${message.author.id}\``)
             .addField(`On the Server`, `\`${message.guild.name}\``)
             .addField("Server ID:", `\`${message.guild.id}\``)
-            .setColor("#FF0000")
+            .setColor("#ff0000")
             .setTimestamp()
                     
         message.client.channels.cache.get(fbChannel).send(embed).then((msg) =>{
+            msg.react('<a:astroz_success:839478588192718898>');
+            msg.react('<a:astroz_error:839478585642713138>');
         }).catch((err)=>{
             throw err;
         });
 
 
         const successembed = new MessageEmbed()
+        .addField("Join Our Support Server", `Click Here To Join Our [Support Server](https://discord.gg/Arqg9kc8r5)`)
         .setTitle("Success!")
-        .setColor("#FF0000")
-        .setDescription(` Your **Bug** is submitted successfully!`)
+        .setColor("#ff0000")
+        .setDescription(`<a:astroz_success:839478588192718898> Your **Bug** is submitted successfully!`)
+        message.react("<a:astroz_success:839478588192718898>");
 
         message.author.send(successembed)
     }
