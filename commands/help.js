195c3e8a131693d9ff0a757d697ae2589355ba7d
SHIVAM0001-FLEX <80771521+SHIVAM0001-FLEX@users.noreply.github.com>
2021-05-14 03:13:02 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -13,7 +13,7 @@ module.exports = {
         .setAuthor("Commands of "+client.user.username, "https://images-ext-1.discordapp.net/external/oVHlY2qCVNh13xNwI0E9Exlw05pSj-Aj8amYa84qoRM/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/822294569223454740/06d90b47826d04bc369fef70fd8b515a.png?width=427&height=427")
         .setColor("#FF0000")
 				.setDescription("Hey my prefix in this server is **+**")
-				.addField("<a:Music:841963243676106752> __**Music Commands**__", "`play`, `skip`, `join`, `leave`, `volume`, `queue`, `np`, `stop`, `loop`, `pause`, `resume`, `lyrics`, `playlist`, `remove`, `shuffle`, `skipto`, `search`, `24/7`", false)
+				.addField("<a:Music:841963243676106752> __**Music Commands**__", "`play`, `skip`, `join`, `leave`, `volume`, `queue`, `np`, `stop`, `loop`, `pause`, `resume`, `lyrics`, `playlist`, `remove`, `shuffle`, `skipto`, `search`, `setvc`", false)
 				.addField("<a:alert:839493679030730764> __**Utility Commands**__", "`ping`, `invite`, `uptime`, `serverinfo`, `stats`, `help`", false)
         .setFooter(`To get info of each command you can do +help [command]`)
 				.addField("<a:BOT:839493686269182009> __**Developer Commands**__", "`eval`, `serverlist`, `setstatus`,", false)
